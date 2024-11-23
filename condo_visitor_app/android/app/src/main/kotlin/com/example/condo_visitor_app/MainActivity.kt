9773722c7fa19fda670e85d1624e4a3d91a650cb
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2024-11-23 16:19:47 -0300

    23/11/24

--- condo_visitor_app/android/app/src/main/kotlin/com/example/condo_visitor_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.condo_visitor_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- condominio_qrcode/android/app/src/main/kotlin/com/example/condominio_qrcode/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.condominio_qrcode
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
